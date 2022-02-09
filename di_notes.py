e3cfd245431c3b698e5316641021c6bfcbc24d14
leopardi2006 <87456430+leopardi2006@users.noreply.github.com>
2022-02-09 22:59:51 +0800

    test02
    return popular movie

--- di_notes.py
+++ di_notes.py
@@ -10,3 +10,30 @@ python -m pytest tests/01_connect_to_neo4j__test.py
 
 #Because this will make sure that class create_app from api/init.py is loaded when importing conftest.py.
 
+
+
+#python -- begin of  working example
+#Create a Person node in the customers database
+def create_person_work(tx, name):
+    return tx.run("CREATE (p:Person {name: $name}) RETURN p",
+        name=name).single()
+
+def create_person(name):
+    # Create a Session for the `people` database
+    session = driver.session(database="people")
+
+    # Create a node within a write transaction
+    record = session.write_transaction(create_person_work,
+                                        name=name)
+
+    # Get the `p` value from the first record
+    person = record["p"]
+
+    # Close the session
+    session.close()
+
+    # Return the property from the node
+    return person["name"]
+#python -- end of working example
+
+
